a8f056642af44f8b41c164ae4507e245bdbe27aa
Farhannr28 <farhannafis281004@gmail.com>
2025-09-30 15:50:50 +0700

    fix: Docker configurations

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths"
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-    server: {
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      // This is the key line that tells Vite what '@' means
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
     host: true,      
     port: 5173,
     strictPort: true
   }
-})
+});
